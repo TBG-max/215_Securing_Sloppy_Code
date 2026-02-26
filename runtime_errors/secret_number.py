c5d83afaa2aa8ee81a42686b3e605e859c8eead8
TBG-max <cristian.perez@stu.vertexacademies.org>
2026-02-26 19:17:57 +0000

    added some code

--- runtime_errors/secret_number.py
+++ runtime_errors/secret_number.py
@@ -8,9 +8,11 @@ print("Autheticating user...")
 pwd = input("What's the secret number? ") # User Input
 
 print()         # Skip Line
-
-if pwd == 333:  # Check authentication
+if not isdigit(pwd):
+    print("Get a load of this guy mistaking a wor for a number. try again.")
+if int(pwd) == 333:  # Check authentication
     print("Success! Opening RESTRICTED APP...")
+
 else:
     print("That's the wrong number.")
 
