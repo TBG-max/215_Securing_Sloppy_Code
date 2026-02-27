29de31668f68859c500ce525733155c6653d7d7b
TBG-max <cristian.perez@stu.vertexacademies.org>
2026-02-27 19:16:57 +0000

    added code

--- runtime_errors/module.py
+++ runtime_errors/module.py
@@ -1,4 +1,28 @@
 
-digits = ["0","1","2","3","4","5","6","7","8","9"]
+
 
 def isdigit(string):
+    digits = ["0","1","2","3","4","5","6","7","8","9"]
+    count = 0
+    index = 0
+    
+    
+    while index <= len(string):
+        number = string[index]
+        
+        if number in digits:
+            count += 1
+
+    
+        
+        
+        index += 1
+        return count
+        
+           
+        
+         
+
+        
+
+
